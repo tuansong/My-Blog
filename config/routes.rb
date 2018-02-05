235f38ff1237d6a016715037f96ae10a65a21f64
Song <trantuansong22@gmail.com>
2018-02-05 13:12:07 +0000

    Fix create, add flash and errors messages, create show

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'pages#homepage'
   get 'pages/about', to: 'pages#about'
+  
+  resources :articles
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
